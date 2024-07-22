71978474176b1b42c8a74de1e62a82f267d41d04
Jhenkar MS <jhenkar5nov3@gmail.com>
2024-07-22 23:34:52 +0530

    deleted all files

--- 6th_sem/FSD/PRG08/project8/app8/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class App8Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app8'
--- 6th_sem/FSD/PRG11/project11/app11/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from app11.models import Course, Student
-
-# Register your models here.
-
-admin.site.register(Student)
-admin.site.register(Course)
